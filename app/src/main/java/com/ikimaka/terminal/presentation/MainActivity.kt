d2b47d7ad879c322f54fcd84145e623f4e101abf
ikimaka <ikimaka@yandex.ru>
2024-08-10 12:19:10 +0300

    Рисуем график

--- app/src/main/java/com/ikimaka/terminal/presentation/MainActivity.kt
+++ app/src/main/java/com/ikimaka/terminal/presentation/MainActivity.kt
@@ -4,25 +4,30 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ikimaka.terminal.ui.theme.TerminalTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             TerminalTheme {
                 val viewModel: TerminalViewModel = viewModel()
                 val screenState = viewModel.state.collectAsState()
-                when(val currentState = screenState.value) {
+
+                when (val currentState = screenState.value) {
+
                     is TerminalScreenState.Initial -> { }
+
                     is TerminalScreenState.Content -> {
-                        Log.d("MainActivityTest", currentState.barList.toString())
+                        Terminal(bars = currentState.barList)
                     }
+
                 }
+
             }
         }
     }
