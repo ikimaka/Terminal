aa4255aa32c978691548670fd97fbe100702898a
ikimaka <ikimaka@yandex.ru>
2024-08-11 09:04:16 +0300

    Масштабирование

--- app/src/main/java/com/ikimaka/terminal/data/ApiService.kt
+++ app/src/main/java/com/ikimaka/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50001&apiKey=G00m4ojxMdILuS6AOxEZuBRDMuiJHoEB")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50001&apiKey=G00m4ojxMdILuS6AOxEZuBRDMuiJHoEB")
     suspend fun loadBars(): Result
 }
