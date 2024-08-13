d764368f70b0b7054899d2725ddf72e3c703d6c2
ikimaka <ikimaka@yandex.ru>
2024-08-13 13:23:52 +0300

    Refactoring and bug fix

--- app/src/main/java/com/ikimaka/terminal/presentation/MainActivity.kt
+++ app/src/main/java/com/ikimaka/terminal/presentation/MainActivity.kt
@@ -4,7 +4,17 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ikimaka.terminal.ui.theme.TerminalTheme
 
